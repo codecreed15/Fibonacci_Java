edf121e7dbe37d186aa0600f6d91d7c7a746d23c
Code Creed <117778961+codecreed15@users.noreply.github.com>
2025-07-05 00:12:07 -0600

    Update Fibonacci.java

--- src/main/java/com/mycompany/fibonacci/Fibonacci.java
+++ src/main/java/com/mycompany/fibonacci/Fibonacci.java
@@ -1,41 +1,30 @@
-package com.mycompany.fibonacci;
-
 import java.util.Scanner;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
-        Scanner L=new Scanner(System.in);
+        Scanner L = new Scanner(System.in);
         
-        int a, r;
-        System.out.println("Ingresa el tamaño de la serie");
-        a=L.nextInt();
+        System.out.println("Ingresa el tamaño de la serie:");
+        int a = L.nextInt();
         
+        int an = 0, ac = 1;
         
-        //valor anterior y valor actual
-        int an=0, ac=0;
         System.out.println("----------Serie de Fibonacci-----------\n");
         
-        for (int i = 1; i < a; i++) {
-            
-            if(i<=1){
-             //Le asigna el indice 1 a los dos para ue oomience de esta manera:
-             // 1,1
-             ac=i;
-             an=i;
-             System.out.print(an+","+ac);
+        for (int i = 0; i < a; i++) {
+            if (i == 0) {
+                System.out.print(an);
+            } else if (i == 1) {
+                System.out.print(", " + ac);
+            } else {
+                int r = an + ac;
+                System.out.print(", " + r);
+                an = ac;
+                ac = r;
             }
-            else if(i>1){
-                //Suma el anterior más el actual y se va haciendo la suceción así
-                //1,1,2,3........
-                r=an + ac;
-                System.out.print(","+r);
-                
-                //le asigna el valor actual al anterior y al actual el resultado
-                an=ac;
-                ac=r;
-            }
-            
         }
+        
+        L.close();
     }
 }
